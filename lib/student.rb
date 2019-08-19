8a15297413e96ec623d84f2910b66e12bd6e7d5e
github username <github email address>
2019-08-19 11:13:52 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,17 +3,25 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = Student.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
-     
+     sql = << -SQL
+      SELECT * 
+      FROM students
+     SQL
     # remember each row should be a new instance of the Student class
+    # do iteration
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+
   end
 
   def self.find_by_name(name)
@@ -22,7 +30,10 @@ class Student
     sql = << -SQL
     SELECT * FROM students WHERE Student.name = ? LIMIT 1
     sql
-
+    DB[:conn].execute (sql, name).map do |row| 
+      self.new_from_db(row)
+    end.first
+  
   end
   
   def save
@@ -50,4 +61,52 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_student_in_grade_9
+    sql = << -SQL
+      SELECT * FROM students 
+      WHERE grade = 9
+
+      # convert to object - call .new_from_db
+
+  end
+
+  def self.students_below_12th_grade
+    sql = << -SQL
+    SELECT * FROM students
+    WHERE grade < 12
+
+    # covert to object - call .new_from_db
+
+  end
+
+  def self.first_X_students_in_grade_10(number_of_students)
+    sql = << -SQL
+    SELECT * FROM students
+    WHERE grade = 10 
+
+    # covvert to object - call .new_from_db
+
+  end
+
+  def self.first_student_in_grade_10
+  
+    sql = << - SQL
+    SELECT * FROM students
+    WHERE grade = 10
+
+    # covert to object - call .new_from_db
+
+
+  end
+
+  def self.all_students_in_grade_X(grade)
+
+    sql == < -SQL
+    SELECT * FROM students
+    WHERE grade 
+
+
+  end
+
 end
